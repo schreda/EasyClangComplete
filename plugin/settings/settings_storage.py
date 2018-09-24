58f0c1727c7f1664020195af6a75ffcffb93864b
schreda <schreda@gmail.com>
2018-09-24 13:09:16 +0200

    fix style

--- plugin/settings/settings_storage.py
+++ plugin/settings/settings_storage.py
@@ -56,8 +56,11 @@ class SettingsStorage:
     GUTTER_MONO_STYLE = "mono"
     GUTTER_DOT_STYLE = "dot"
     NONE_STYLE = "none"
-    
-    GUTTER_STYLES = [GUTTER_COLOR_STYLE, GUTTER_MONO_STYLE, GUTTER_DOT_STYLE, NONE_STYLE]
+
+    GUTTER_STYLES = [GUTTER_COLOR_STYLE,
+                     GUTTER_MONO_STYLE, 
+                     GUTTER_DOT_STYLE, 
+                     NONE_STYLE]
 
     # refer to Preferences.sublime-settings for usage explanation
     NAMES_ENUM = [
